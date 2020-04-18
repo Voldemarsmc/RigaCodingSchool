da3a8d3cc984637f4cac0a71aade482113ccc030
Voldemārs <33371287+Voldemarsmc@users.noreply.github.com>
2020-04-18 22:23:07 +0300

    Add files via upload

---
+++ uzd1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace majasdarbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("4. uzdevums");
+            int a = 3;
+            int b = 9;
+            int temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+            Console.WriteLine("a = " +a);
+            Console.WriteLine("b = " +b);
+        }
+    }
+}
---
+++ uzd2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace majasdarbs2_uzd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.WriteLine("lūdzu ievadiet savu vārdu :");
+            String ievade = Console.ReadLine();
+            String combined = "sveiki, " + ievade;
+            Console.WriteLine(combined);
+
+
+            Console.WriteLine("ievadīt 3 simbolu virknes");
+            Console.WriteLine("lūdzu ievadiet pirmo virkni :");
+            String a = Console.ReadLine();
+            Console.WriteLine("lūdzu ievadiet otro virkni :");
+            String b = Console.ReadLine();
+            Console.WriteLine("lūdzu ievadiet trešo virkni :");
+            String c = Console.ReadLine();
+
+            string kopa = a + " " + b + " " + c;
+            Console.WriteLine(kopa);
+        }
+            
+    }
+}
