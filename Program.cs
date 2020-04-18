49e0f310449a5466acb1ec54c53303cea0dcdbb0
Voldemārs <33371287+Voldemarsmc@users.noreply.github.com>
2020-04-18 22:20:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace majasdarbs3_uzd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3. uzdevums");
+
+            int a, b, c;
+            Console.WriteLine("ievadiet a vērtību :");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("ievadiet b vērtību :");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("ievadiet c vērtību :");
+            c = int.Parse(Console.ReadLine());
+
+            if (c < a & b < a)
+            {
+                Console.WriteLine("augstākā vērtība ir a : " + a);
+            }
+            else if (c < b & a < b)
+            {
+                Console.WriteLine("augstākā vērtība ir b : " + b);
+            }
+            else if (b < c & a < c)
+            {
+                Console.WriteLine("augstākā vērtība ir c : " + c);
+            }
+            else
+            {
+                Console.WriteLine("something went wrong");
+            }
+        }
+    }
+}
